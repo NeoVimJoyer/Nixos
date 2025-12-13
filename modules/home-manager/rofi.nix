14ab1e2b0b7d997a4c572169798743fa30d92b63
Main User <thecallummckenna@proton.me>
2025-12-12 23:04:49 -0700

    rofi theme

--- modules/home-manager/rofi.nix
+++ modules/home-manager/rofi.nix
@@ -1,7 +1,28 @@
-{ ... }:
+{ config, ... }:
 
 {
   programs.rofi = {
     enable = true;
+    location = "top-left";
+    theme = let
+      inherit (config.lib.formats.rasi) mkLiteral;
+    in{
+      "*" = {
+        background-color = mkLiteral "#000000";
+        text-color = mkLiteral "#7dfdfe";
+        width = 512;
+      };
+
+      "#inputbar" = {
+        children = map mkLiteral [ "prompt" "entry" ];
+      };
+
+      "#textbox-prompt-colon" = {
+        expand = false;
+        str = ":";
+        margin = mkLiteral "5px";
+        text-color = mkLiteral "@text-color";
+      };
+    };
   };
 }
